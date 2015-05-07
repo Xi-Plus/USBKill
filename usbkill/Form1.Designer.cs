f09081f2c0b8b931ca34cbc3338a5dffeb449c82
Xi-Plus <a2093064@yahoo.com.tw>
2015-05-07 19:55:53 +0800

    basic

---
+++ usbkill/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace usbkill
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton0 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 89);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(264, 160);
+            this.listBox1.TabIndex = 0;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(54, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 16);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.Text = "鎖定";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(160, 12);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(47, 16);
+            this.radioButton3.TabIndex = 1;
+            this.radioButton3.Text = "登出";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(213, 12);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(47, 16);
+            this.radioButton4.TabIndex = 1;
+            this.radioButton4.Text = "關機";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(107, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "休眠";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton0
+            // 
+            this.radioButton0.AutoSize = true;
+            this.radioButton0.Checked = true;
+            this.radioButton0.Location = new System.Drawing.Point(12, 12);
+            this.radioButton0.Name = "radioButton0";
+            this.radioButton0.Size = new System.Drawing.Size(35, 16);
+            this.radioButton0.TabIndex = 1;
+            this.radioButton0.TabStop = true;
+            this.radioButton0.Text = "無";
+            this.radioButton0.UseVisualStyleBackColor = true;
+            this.radioButton0.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton0);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "USB Kill";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton0;
+    }
+}
+
---
+++ usbkill/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace usbkill
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ShowInfo();
+        }
+        int shutdowntype = 0;
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_DEVICECHANGE = 0x219;
+            const int DBT_DEVICEARRIVAL = 0x8000;
+            const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+
+            object ojb = new object();
+            try
+            {
+                // WM_DEVICECHANGE Message : 電腦硬體裝置改變時產生的訊息
+                if (m.Msg == WM_DEVICECHANGE)
+                {
+                    switch (m.WParam.ToInt32())
+                    {
+                        case WM_DEVICECHANGE:
+                            break;
+                        // DBT_DEVICEARRIVAL Event : 裝置插入並且可以使用時，產生的系統訊息
+                        case DBT_DEVICEARRIVAL:
+                        // DBT_DEVICEREMOVECOMPLETE Event : 裝置卸載或移除時產生的系統訊息
+                        case DBT_DEVICEREMOVECOMPLETE:
+                            DeviceChange();
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            base.WndProc(ref m);
+        }
+
+        private void DeviceChange()
+        {
+            ShutDown();
+            ShowInfo();
+        }
+        private void ShowInfo()
+        {
+            String[] shutdownname = { "無", "鎖定", "休眠", "登出", "關機" };
+            if (radioButton0.Checked)
+            {
+                shutdowntype = 0;
+            }
+            else if (radioButton1.Checked)
+            {
+                shutdowntype = 1;
+            }
+            else if (radioButton2.Checked)
+            {
+                shutdowntype = 2;
+            }
+            else if (radioButton3.Checked)
+            {
+                shutdowntype = 3;
+            }
+            else if (radioButton4.Checked)
+            {
+                shutdowntype = 4;
+            }
+            listBox1.Items.Clear();
+            listBox1.Items.Add("USB Kill 模式已改為：" + shutdownname[shutdowntype]);
+            foreach (DriveInfo di in DriveInfo.GetDrives())
+            {
+                if (di.DriveType == DriveType.Removable)
+                {
+                    listBox1.Items.Add("偵測到" + di.Name + "抽取式存放裝置");
+                }
+            }
+        }
+        private void ShutDown()
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            switch (shutdowntype)
+            {
+                case 1:
+                    p.StandardInput.WriteLine("rundll32.exe user32.dll,LockWorkStation");
+                    break;
+                case 2:
+                    p.StandardInput.WriteLine("shutdown -h");
+                    break;
+                case 3:
+                    p.StandardInput.WriteLine("shutdown -l");
+                    break;
+                case 4:
+                    p.StandardInput.WriteLine("shutdown -s -t 0");
+                    break;
+            }
+            p.StandardInput.WriteLine("exit");
+            string sOutput = p.StandardOutput.ReadToEnd();
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            ShowInfo();
+        }
+    }
+}
